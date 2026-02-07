33c66f7c0aae4a4b9b2a07dc3e20dbf8cb9d4eec
Oğuzhan Gökcekoca <oguzhangokcekoca@Oguzhans-MacBook-Air.local>
2026-02-07 18:44:35 +0300

    refactor

--- .next/static/9a3ASr9nrnqpsxI49FKw4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/d7eeaac4-91a42bc9fc7e6065.js","static/chunks/594-2c184461f4c28c03.js","static/chunks/pages/index-8a0abf8e836eeb1f.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
